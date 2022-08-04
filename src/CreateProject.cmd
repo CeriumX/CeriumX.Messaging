22620ba64a6e0419189201db1b04f761fd8ee579
CockroachSoulful <cockroach888@outlook.com>
2022-08-04 16:50:24 +0800

    feat: 优化项目创建脚本内容。

--- src/CreateProject.cmd
+++ src/CreateProject.cmd
@@ -12,7 +12,7 @@ cd /d %basedir%
 @echo.
 @echo\&echo  ---------- 解决方案 ----------
 
-dotnet new sln -n  CRS2TBBT4CeriumX.Messaging
+dotnet new sln -n CRS2TBBT4CeriumX.Messaging
 
 
 
@@ -21,9 +21,10 @@ dotnet new sln -n  CRS2TBBT4CeriumX.Messaging
 @echo.
 @echo.
 @echo.
-@echo\&echo  ---------- 引导程序 & Bootloader 1 ----------
-dotnet new classlib -lang "C#" -f net5.0 -n CeriumX.Framework.Bootloader -o CeriumX.Framework.Bootloader\src
-dotnet sln CRS2TBBT4CeriumX.Messaging.sln add --in-root CeriumX.Framework.Bootloader\src
+@echo\&echo  ---------- 引导程序 & Bootstrapper 1 ----------
+
+dotnet new console -lang C# -f net6.0 -n CeriumX.Messaging.Bootstrapper -o samples/CeriumX.Messaging.Bootstrapper/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add -s samples samples/CeriumX.Messaging.Bootstrapper/src
 
 
 
@@ -32,37 +33,63 @@ dotnet sln CRS2TBBT4CeriumX.Messaging.sln add --in-root CeriumX.Framework.Bootlo
 @echo.
 @echo.
 @echo.
-@echo\&echo  ---------- 项目类别 ﹫ ProjectCategory 10 ----------
+@echo\&echo  ---------- 接口抽象 ﹫ Abstractions 1 ----------
 
-dotnet new web -lang C# -f net6.0 -n LibrayName -o ProjectCategory/LibrayName/src
-dotnet sln SolutionName.sln add -s ProjectCategory ProjectCategory/LibrayName/src
+dotnet new classlib -lang "C#" -f net6.0 -n CeriumX.Messaging.Abstractions -o CeriumX.Messaging.Abstractions/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add --in-root CeriumX.Messaging.Abstractions/src
 
-dotnet new blazorserver -lang C# -f net6.0 -n LibrayName -o ProjectCategory/LibrayName/src
-dotnet sln SolutionName.sln add -s ProjectCategory ProjectCategory/LibrayName/src
 
-dotnet new blazorwasm -lang C# -f net6.0 -n LibrayName -o ProjectCategory/LibrayName/src
-dotnet sln SolutionName.sln add -s ProjectCategory ProjectCategory/LibrayName/src
 
-dotnet new mvc -lang C# -f net6.0 -n LibrayName -o ProjectCategory/LibrayName/src
-dotnet sln SolutionName.sln add -s ProjectCategory ProjectCategory/LibrayName/src
+@echo.
+@echo.
+@echo.
+@echo\&echo  ---------- 消息队列服务总线 ﹫ ServiceBus 1 ----------
 
-dotnet new webapi -lang "C#" -f net6.0 --exclude-launch-settings --no-https -n LibrayName -o LibrayName/src
-dotnet sln SolutionName.sln add --in-root LibrayName/src
+dotnet new classlib -lang "C#" -f net6.0 -n CeriumX.Messaging.ServiceBus -o ServiceBus/CeriumX.Messaging.ServiceBus/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add -s ServiceBus ServiceBus/CeriumX.Messaging.ServiceBus/src
 
-dotnet new winforms -lang "C#" -f net6.0 -n LibrayName -o samples/LibrayName/src
-dotnet sln SolutionName.sln add -s samples samples/LibrayName/src
 
-dotnet new wpf -lang "C#" -f net6.0 -n LibrayName -o ProjectCategory/LibrayName/src
-dotnet sln SolutionName.sln add -s ProjectCategory ProjectCategory/LibrayName/src
 
-dotnet new console -lang C# -f net6.0 -n LibrayName -o samples/LibrayName/src
-dotnet sln SolutionName.sln add -s samples samples/LibrayName/src
+@echo.
+@echo.
+@echo.
+@echo\&echo  ---------- 本地消息队列 ﹫ LocalMQ 2 ----------
 
-dotnet new classlib -lang "C#" -f net6.0 -n LibrayName -o LibrayName/src
-dotnet sln SolutionName.sln add --in-root LibrayName/src
+dotnet new classlib -lang "C#" -f net6.0 -n CeriumX.Messaging.LocalMQ -o LocalMQ/CeriumX.Messaging.LocalMQ/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add -s LocalMQ LocalMQ/CeriumX.Messaging.LocalMQ/src
 
-dotnet new classlib -lang "C#" -f net6.0 -n LibrayName -o ProjectCategory/LibrayName/src
-dotnet sln SolutionName.sln add -s ProjectCategory ProjectCategory/LibrayName/src
+dotnet new classlib -lang "C#" -f net6.0 -n CeriumX.Messaging.LocalMQPlus -o LocalMQ/CeriumX.Messaging.LocalMQPlus/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add -s LocalMQ LocalMQ/CeriumX.Messaging.LocalMQPlus/src
+
+
+
+@echo.
+@echo.
+@echo.
+@echo\&echo  ---------- MQTT消息队列 ﹫ MQTT 1 ----------
+
+dotnet new classlib -lang "C#" -f net6.0 -n CeriumX.Messaging.MQTT -o MQTT/CeriumX.Messaging.MQTT/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add -s MQTT MQTT/CeriumX.Messaging.MQTT/src
+
+
+
+@echo.
+@echo.
+@echo.
+@echo\&echo  ---------- RabbitMQ消息队列 ﹫ RabbitMQ 1 ----------
+
+dotnet new classlib -lang "C#" -f net6.0 -n CeriumX.Messaging.RabbitMQ -o RabbitMQ/CeriumX.Messaging.RabbitMQ/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add -s RabbitMQ RabbitMQ/CeriumX.Messaging.RabbitMQ/src
+
+
+
+@echo.
+@echo.
+@echo.
+@echo\&echo  ---------- Kafka消息队列 ﹫ Kafka 1 ----------
+
+dotnet new classlib -lang "C#" -f net6.0 -n CeriumX.Messaging.Kafka -o Kafka/CeriumX.Messaging.Kafka/src
+dotnet sln CRS2TBBT4CeriumX.Messaging.sln add -s Kafka Kafka/CeriumX.Messaging.Kafka/src
 
 
 
